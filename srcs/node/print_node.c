487893ffb7e7cf3473554b632d8c1676e5a53541
Arno Voyer <arno.voyer@gmail.com>
2026-02-23 01:27:01 +0100

    add more info on printf_node

--- srcs/node/print_node.c
+++ srcs/node/print_node.c
@@ -10,6 +10,19 @@ void	print_node(void *data)
 
 	ft_printf("node \"%s\" : %d:%d\n", node->name, node->x, node->y);
 	
+	switch (node->type)
+	{
+	case START:
+		ft_printf("type: Start\n");
+		break;
+	case END:
+		ft_printf("type: End\n");
+		break;
+	case NORMAL:
+		ft_printf("type: Normal\n");
+		break;
+	
+	}
 	ft_printf("links: ");
 	unsigned int i = 0;
 	for (; i < node->links->size - 1; i++)
